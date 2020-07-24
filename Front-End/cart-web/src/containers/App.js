13456998b8c8baf80b14b2b42ff2fcc6590a8cda
Nguyễn Quốc Cường - CMC Global DU1 <nqcuong@ldap.cmcglobal.com.vn>
2020-07-24 14:06:46 +0700

    Initial commit

--- Front-End/cart-web/src/containers/App.js
+++ Front-End/cart-web/src/containers/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import 'antd/dist/antd.css';
-import HomePage from './HomePage';
 import { Switch, Route } from 'react-router-dom';
 
 
 function App() {
   return (
     <Switch>
-      <Route path="/" render={() => <HomePage />} />
     </Switch>
   );
 }
