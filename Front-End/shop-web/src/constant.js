7c3bfe6d30143eec35092d99716b9ec4c4620b8a
Nguyen Minh Duc <nmduc@cmc.com.vn>
2020-07-27 11:35:10 +0700

    push

---
+++ Front-End/shop-web/src/constant.js
@@ -0,0 +1,2 @@
+export const ENDPOINT = 'https://5c6521b719df280014b6267d.mockapi.io/api';
+export const GET_PRODUCTS_API = '/products';
--- Front-End/shop-web/src/containers/Homepage/index.js
+++ Front-End/shop-web/src/containers/Homepage/index.js
@@ -15,7 +15,7 @@ export default function HomePage() {
 
     // componentDidMount
     useEffect(() => {
-        dispatch(getAllProducts());
+        // dispatch(getAllProducts());
     }, [])
 
 
