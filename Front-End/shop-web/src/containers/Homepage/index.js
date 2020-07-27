df129dd7e96b7647911e3e1b192f165b1091d5fb
Nguyen Minh Duc <nmduc@cmc.com.vn>
2020-07-27 11:11:24 +0700

    build items category

--- Front-End/shop-web/src/containers/Homepage/index.js
+++ Front-End/shop-web/src/containers/Homepage/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import ProductItem from '../../components/ProductItem/index'
 
 export default function HomePage() {
     return (
         <div>
             <p>HOME PAGE</p>
+            <ProductItem/>
         </div>
     )
 }
