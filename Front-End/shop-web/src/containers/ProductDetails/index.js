79bfceee2863e94fe5754f366739721f819db07a
Nguyen Minh Duc <nmduc@cmc.com.vn>
2020-07-28 16:25:22 +0700

    add pr to cart

--- Front-End/shop-web/src/containers/ProductDetails/index.js
+++ Front-End/shop-web/src/containers/ProductDetails/index.js
@@ -4,10 +4,9 @@ import ProductRow from "./ProductsRow";
 import { useHistory } from "react-router-dom";
 import axiosService from "../../utils/axiosService";
 import { ENDPOINT, GET_PRODUCTS_API } from "../../constant";
-import Loading from '../../components/Loading';
+import Loading from "../../components/Loading";
 
 export default function Productdetails() {
-
   const history = useHistory();
   // console.log("history :>> ", history);
   const type = history.location.state.type;
@@ -22,9 +21,11 @@ export default function Productdetails() {
     const fetchData = async () => {
       if (type === "home") {
         console.log(`go home`);
-        const res = await axiosService.get(`${ENDPOINT}${GET_PRODUCTS_API}/${id}`)
-        console.log('res :>> ', res);
-        setProduct(res.data)
+        const res = await axiosService.get(
+          `${ENDPOINT}${GET_PRODUCTS_API}/${id}`
+        );
+        console.log("res :>> ", res);
+        setProduct(res.data);
         setLoading(false);
       } else {
         console.log(`go category`);
@@ -34,13 +35,14 @@ export default function Productdetails() {
     fetchData();
   }, []);
 
-  console.log('product :>> ', product);
+  console.log("product :>> ", product);
 
   return (
     <div>
       {loading && <Loading />}
       <div className="container">
         <ProductRow
+          product={product}
           image={product.image}
           titleProduct={product.name}
           PriceProduct={product.price}
