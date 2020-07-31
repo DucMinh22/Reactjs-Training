35971dd312fc5480ca21d932cdf25a5ad54b9a81
Nguyen Minh Duc <nmduc@cmc.com.vn>
2020-07-31 13:48:17 +0700

    fixed search function

--- Front-End/shop-web/src/components/Banner/index.js
+++ Front-End/shop-web/src/components/Banner/index.js
@@ -1,37 +1,7 @@
 import React from "react";
 import "./index.scss";
-import { Input } from "antd";
-import { useSelector, useDispatch } from "react-redux";
-import axiosService from "../../utils/axiosService";
-import { searchProduct } from "../../action/action";
-import { useHistory } from "react-router-dom";
-import { ENDPOINT, GET_PRODUCTS_API } from "../../constant";
-const { Search } = Input;
 
 export default function Banner() {
-  const dispatch = useDispatch();
-  const stateSearch = useSelector((state) => state.products);
-  const search = stateSearch.searchProducts;
-
-  const history = useHistory();
-
-  const handleNavigateSearch = (search) => {
-    history.push({
-      pathname: `/search`,
-      state: { key: search },
-    });
-  };
-  const handleSearch = (value) => {
-    axiosService
-      .get(`${ENDPOINT}${GET_PRODUCTS_API}?search=${value}`)
-      .then((res) => {
-        dispatch(searchProduct(res.data));
-        handleNavigateSearch(value);
-      })
-      .catch((error) => {
-        console.log("Error fetching and parsing data", error);
-      });
-  };
   return (
     <div>
       <div className="img-banner">
