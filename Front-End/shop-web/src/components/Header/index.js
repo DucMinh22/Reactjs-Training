bc0c29427ce8ba77527792f14c4091c11353345d
Nguyen Minh Duc <nmduc@cmc.com.vn>
2020-07-31 16:48:35 +0700

    admin login

--- Front-End/shop-web/src/components/Header/index.js
+++ Front-End/shop-web/src/components/Header/index.js
@@ -13,163 +13,190 @@ import axiosService from "../../utils/axiosService";
 import { ENDPOINT, GET_PRODUCTS_API } from "../../constant";
 
 const MENU = [
-    {
-        id: 1,
-        name: "Home",
-        to: "/",
-        exact: true,
-    },
-    {
-        id: 2,
-        name: "Category",
-        to: "/category",
-        exact: false,
-    },
-    {
-        id: 3,
-        name: "Login",
-        to: "#",
-        exact: false,
-    },
-    {
-        id: 4,
-        name: "Cart",
-        to: "/cart",
-        exact: false,
-    },
+  {
+    id: 1,
+    name: "Home",
+    to: "/",
+    exact: true,
+    isAdmin: false,
+  },
+  {
+    id: 2,
+    name: "Category",
+    to: "/category",
+    exact: false,
+    isAdmin: false,
+  },
+  {
+    id: 3,
+    name: "Login",
+    to: "#",
+    exact: false,
+    isAdmin: true,
+  },
+  {
+    id: 4,
+    name: "Cart",
+    to: "/cart",
+    exact: false,
+    isAdmin: false,
+  },
 ];
 
 export default function Header() {
-    const [visible, setVisible] = useState(false);
-    const history = useHistory();
-    const cartReducer = useSelector((state) => state.cart);
-    const { cartProducts } = cartReducer;
-    const { Search } = Input;
+  const [visible, setVisible] = useState(false);
+  const history = useHistory();
+  const cartReducer = useSelector((state) => state.cart);
+  const { cartProducts } = cartReducer;
+  const { Search } = Input;
+  const dispatch = useDispatch();
+  const stateSearch = useSelector((state) => state.products);
+  const search = stateSearch.searchProduct;
 
-    const showModal = () => {
-        setVisible(true);
-    };
-    const handleSignOut = () => {
-        localStorage.removeItem("name");
-    };
-    const handleCancel = () => {
-        setVisible(false);
-    };
-    const dispatch = useDispatch();
-    const stateSearch = useSelector((state) => state.products);
-    const search = stateSearch.searchProduct;
+  // if (localStorage.getItem("name")) {
+  //   MENU.splice(1, 3);
+  // }
 
-    const handleNavigateSearch = (search) => {
-        history.push({
-            pathname: `/search`,
-            state: { key: search },
-        });
-    };
-    const handleSearch = (value) => {
-        axiosService
-            .get(`${ENDPOINT}${GET_PRODUCTS_API}?search=${value}`)
-            .then((res) => {
-                dispatch(searchProduct(res.data));
-                handleNavigateSearch(value);
-            })
-            .catch((error) => {
-                console.log("Error fetching and parsing data", error);
-            });
-    };
-    return (
-        <Affix>
-            <Row className="header" align='middle'>
-                <Col span={5}>
-                    <div className="logo">
-                        <a href="/">
-                            <img src={LOGO} className="logo__img" alt="Logo" />
-                        </a>
-                    </div>
-                </Col>
-                <Col span={19}>
-                    <Row align="middle">
-                        <Col span={12}>
-                            <Search
-                                placeholder="input search text"
-                                enterButton="Search"
-                                size="large"
-                                onSearch={handleSearch}
+  const showModal = () => {
+    setVisible(true);
+  };
+  const handleSignOut = () => {
+    localStorage.removeItem("name");
+  };
+  const handleCancel = () => {
+    setVisible(false);
+  };
+
+  const handleNavigateSearch = (search) => {
+    history.push({
+      pathname: `/search`,
+      state: { key: search },
+    });
+  };
+
+  const handleSearch = (value) => {
+    axiosService
+      .get(`${ENDPOINT}${GET_PRODUCTS_API}?search=${value}`)
+      .then((res) => {
+        dispatch(searchProduct(res.data));
+        handleNavigateSearch(value);
+      })
+      .catch((error) => {
+        console.log("Error fetching and parsing data", error);
+      });
+  };
+  return (
+    <Affix>
+      <Row className="header" align="middle">
+        <Col span={5}>
+          <div className="logo">
+            <a href="/">
+              <img src={LOGO} className="logo__img" alt="Logo" />
+            </a>
+          </div>
+        </Col>
+        <Col span={19}>
+          <Row align="middle">
+            <Col span={12}>
+              <Search
+                placeholder="input search text"
+                enterButton="Search"
+                size="large"
+                onSearch={handleSearch}
+              />
+            </Col>
+            <Col span={12}>
+              <div className="menu">
+                <ul className="menuList">
+                  {MENU?.map((item) => (
+                    <li
+                      key={item.id}
+                      style={
+                        localStorage.getItem("name") === "admin"
+                          ? item.isAdmin
+                            ? { display: "block" }
+                            : { display: "none" }
+                          : { display: "block" }
+                      }
+                    >
+                      <NavLink
+                        id={item.name === "Login" ? "login" : ""}
+                        exact={item.exact}
+                        className="menuList__item"
+                        activeClassName="menuList__item-active "
+                        to={item.to}
+                      >
+                        {item.name === "Login" && localStorage.getItem("name")
+                          ? localStorage.getItem("name")
+                          : item.name}
+
+                        {item.name === "Login" && (
+                          <div className="itemIcon Login">
+                            <UserOutlined
+                              style={{ fontSize: "20px", marginLeft: "10px" }}
                             />
-                        </Col>
-                        <Col span={12}>
-                            <div className="menu">
-                                <ul className="menuList">
-                                    {MENU?.map((item) => (
-                                        <li key={item.id}>
-                                            <NavLink
-                                                id={item.name === "Login" ? "login" : ""}
-                                                exact={item.exact}
-                                                className="menuList__item"
-                                                activeClassName="menuList__item-active "
-                                                to={item.to}
-                                            >
-                                                {item.name === "Login" && localStorage.getItem("name")
-                                                    ? localStorage.getItem("name")
-                                                    : item.name}
-                                                {item.name === "Login" && (
-                                                    <div className="itemIcon Login">
-                                                        <UserOutlined
-                                                            style={{ fontSize: "20px", marginLeft: "10px" }}
-                                                        />
-                                                        <div className="">
-                                                            <div class="toggler Userstyle__UserDropDown-sc-6e6am-5 cVRwHa">
-                                                                {localStorage.getItem("name") ? (
-                                                                    <button
-                                                                        onClick={handleSignOut}
-                                                                        class="Userstyle__UserDropDownButton-sc-6e6am-10 dYkBsI"
-                                                                    > Sign out </button>
-                                                                ) : (
-                                                                        <>
-                                                                            <button
-                                                                                onClick={showModal}
-                                                                                class="Userstyle__UserDropDownButton-sc-6e6am-10 dYkBsI"
-                                                                            >Sign in</button>
-                                                                            <button className="Userstyle__UserDropDownButton-sc-6e6am-10 dYkBsI">
-                                                                                Sign up </button>
-                                                                        </>
-                                                                    )}
-                                                            </div>
-                                                        </div>
-                                                        <Modal
-                                                            title="Login"
-                                                            visible={visible}
-                                                            onCancel={handleCancel}
-                                                        >
-                                                            <Login onCancel={handleCancel} />
-                                                        </Modal>
-                                                    </div>
-                                                )}
-                                                {item.name === "Cart" && (
-                                                    <div classNameName="itemIcon">
-                                                        <Badge
-                                                            count={cartProducts.length}
-                                                            style={{ backgroundColor: "#7fad39" }}
-                                                        >
-                                                            <ShoppingCartOutlined
-                                                                style={{ fontSize: "20px", marginLeft: "10px" }}
-                                                            />
-                                                        </Badge>
-                                                    </div>
-                                                )}
-                                            </NavLink>
-                                            <div
-                                                className={`${
-                                                    history.location.pathname === item.to ? "hr-active" : "hr"
-                                                    }`}
-                                            ></div>
-                                        </li>
-                                    ))}
-                                </ul>
+                            <div className="">
+                              <div class="toggler Userstyle__UserDropDown-sc-6e6am-5 cVRwHa">
+                                {localStorage.getItem("name") ? (
+                                  <button
+                                    onClick={handleSignOut}
+                                    class="Userstyle__UserDropDownButton-sc-6e6am-10 dYkBsI"
+                                  >
+                                    {" "}
+                                    Sign out{" "}
+                                  </button>
+                                ) : (
+                                  <>
+                                    <button
+                                      onClick={showModal}
+                                      class="Userstyle__UserDropDownButton-sc-6e6am-10 dYkBsI"
+                                    >
+                                      Sign in
+                                    </button>
+                                    <button className="Userstyle__UserDropDownButton-sc-6e6am-10 dYkBsI">
+                                      Sign up{" "}
+                                    </button>
+                                  </>
+                                )}
+                              </div>
                             </div>
-                        </Col>
-                    </Row>
-                </Col>
-            </Row>
-        </Affix>
-    );
+                            <Modal
+                              title="Login"
+                              visible={visible}
+                              onCancel={handleCancel}
+                            >
+                              <Login onCancel={handleCancel} />
+                            </Modal>
+                          </div>
+                        )}
+                        {item.name === "Cart" && (
+                          <div classNameName="itemIcon">
+                            <Badge
+                              count={cartProducts.length}
+                              style={{ backgroundColor: "#7fad39" }}
+                            >
+                              <ShoppingCartOutlined
+                                style={{ fontSize: "20px", marginLeft: "10px" }}
+                              />
+                            </Badge>
+                          </div>
+                        )}
+                      </NavLink>
+                      <div
+                        className={`${
+                          history.location.pathname === item.to
+                            ? "hr-active"
+                            : "hr"
+                        }`}
+                      ></div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </Affix>
+  );
 }
