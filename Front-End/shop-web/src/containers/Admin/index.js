e79d5a338c6b2469288da68188daa1fcd1909c88
Nguyen Minh Duc <nmduc@cmc.com.vn>
2020-08-05 17:32:41 +0700

    push

--- Front-End/shop-web/src/containers/Admin/index.js
+++ Front-End/shop-web/src/containers/Admin/index.js
@@ -1,77 +1,82 @@
-import React, { useEffect, useState, useCallback } from "react"
-import { Table, Input, Row, Col } from "antd"
-import {
-    getAllBillsID,
-    searchbillsByName,
-} from "../../action/action"
-import { ENDPOINT, GET_BILL_API } from "../../constant"
-import axiosService from "../../utils/axiosService"
-import { useDispatch, useSelector } from "react-redux"
+import React, { useEffect, useState, useCallback } from "react";
+import { Table, Input, Row, Col } from "antd";
+import { getAllBillsID, searchbillsByName } from "../../action/action";
+import { ENDPOINT, GET_BILL_API } from "../../constant";
+import axiosService from "../../utils/axiosService";
+import { useDispatch, useSelector } from "react-redux";
 
-import "./index.scss"
-import { useTranslation } from "react-i18next"
-import TableColumns from "./adminColumns"
+import "./index.scss";
+import { useTranslation } from "react-i18next";
+import TableColumns from "./adminColumns";
+import Loading from "../../components/Loading";
 
 export default function Admin() {
-    const [loading, setLoading] = useState(false);
-    const dispatch = useDispatch();
-    const { Search } = Input;
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
+  const { Search } = Input;
 
-    const stateBills = useSelector((state) => state.products);
-    const { bills } = stateBills;
-    const { t } = useTranslation('common');
+  const stateBills = useSelector((state) => state.products);
+  const { bills } = stateBills;
 
-    // componentDidMount
-    useEffect(() => {
-        const fetchData = async () => {
-            const res = await axiosService.get(`${ENDPOINT}${GET_BILL_API}`);
-            dispatch(getAllBillsID(res.data));
-        };
-        fetchData();
-    }, [dispatch]);
+  const { t } = useTranslation("common");
 
-    const handleSearch = useCallback((value) => {
-        setLoading(true);
-        axiosService
-            .get(`${ENDPOINT}${GET_BILL_API}?search=${value}`)
-            .then((res) => {
-                dispatch(searchbillsByName(res.data));
-            })
-            .catch((error) => {
-                console.log("Error fetching and parsing data", error);
-            })
-            .finally(() => setLoading(false));
-    }, [dispatch])
+  // componentDidMount
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      const res = await axiosService.get(`${ENDPOINT}${GET_BILL_API}`);
+      dispatch(getAllBillsID(res.data));
+      setLoading(false);
+    };
+    fetchData();
+  }, [dispatch]);
 
+  const handleSearch = useCallback(
+    (value) => {
+      setLoading(true);
+      axiosService
+        .get(`${ENDPOINT}${GET_BILL_API}?search=${value}`)
+        .then((res) => {
+          dispatch(searchbillsByName(res.data));
+        })
+        .catch((error) => {
+          console.log("Error fetching and parsing data", error);
+        })
+        .finally(() => setLoading(false));
+    },
+    [dispatch]
+  );
 
-    function onChange(pagination, filters, sorter, extra) {
-        console.log("params", pagination, filters, sorter, extra);
-    }
+  function onChange(pagination, filters, sorter, extra) {
+    console.log("params", pagination, filters, sorter, extra);
+  }
 
-    const columns = TableColumns();
+  const columns = TableColumns();
 
-    return (
-        <div>
-            <Row justify="space-between" align="middle">
-                <Col span={10}>
-                    <h2 style={{ padding: "30px 0" }}>{t(`billpage.title`)}</h2>
-                </Col>
-                <Col span={10}>
-                    <Search
-                        placeholder={t(`searchPlaceholder`)}
-                        enterButton={t(`search`)}
-                        size="large"
-                        onSearch={handleSearch}
-                        loading={loading}
-                    />
-                </Col>
-            </Row>
-            <Table
-                columns={columns}
-                dataSource={bills.map(item => ({ ...item, key: item.id }))}
-                onChange={onChange}
-                rowClassName="bills-row"
-            />
-        </div>
-    );
+  return (
+    <div>
+      {/* {loading && <Loading />} */}
+      <Row justify="space-between" align="middle">
+        <Col span={10}>
+          <h2 style={{ padding: "30px 0" }}>{t(`billpage.title`)}</h2>
+        </Col>
+        <Col span={10}>
+          <Search
+            placeholder={t(`searchPlaceholder`)}
+            enterButton={t(`search`)}
+            size="large"
+            onSearch={handleSearch}
+            loading={loading}
+          />
+        </Col>
+      </Row>
+      <Table
+        columns={columns}
+        dataSource={bills.map((item) => ({ ...item, key: item.id }))}
+        onChange={onChange}
+        rowClassName="bills-row"
+        loading={loading}
+      />
+    </div>
+  );
 }
--- Front-End/shop-web/src/containers/WarehouseDetailpage/index.js
+++ Front-End/shop-web/src/containers/WarehouseDetailpage/index.js
@@ -1,198 +1,194 @@
 import React, { useEffect, useState } from "react";
 import "./index.scss";
 import axiosService from "../../utils/axiosService";
-import {
-    ENDPOINT,
-    GET_PRODUCTS_API,
-} from "../../constant";
+import { ENDPOINT, GET_PRODUCTS_API } from "../../constant";
 import { Col, Row, message } from "antd";
 import {
-    getInfoProducts,
-    getAllCategories,
-    UpdateProducts,
+  getInfoProducts,
+  getAllCategories,
+  UpdateProducts,
 } from "../../action/action";
 import { useDispatch, useSelector } from "react-redux";
 import { useRouteMatch } from "react-router-dom";
 import ModalInput from "../../components/ModalInput";
 import { useTranslation } from "react-i18next";
 import Loading from "../../components/Loading";
-import Button from '../../components/Button';
+import Button from "../../components/Button";
 import * as _ from "lodash";
 import { isRequired, isTypeNumber } from "../../utils/validation";
 
 function WarehouseDetailPage() {
-    const [loading, setLoading] = useState(false);
-    const [newProduct, setNewProduct] = useState({
-        name: "",
-        category: "",
-        price: "",
-        supplier: "",
-        quantity: "",
-        description: "",
+  const [loading, setLoading] = useState(false);
+  const [newProduct, setNewProduct] = useState({
+    name: "",
+    category: "",
+    price: "",
+    supplier: "",
+    quantity: "",
+    description: "",
+  });
+  const dispatch = useDispatch();
+  const match = useRouteMatch();
+  const { t } = useTranslation("common");
+  const { id } = match.params;
+  const allcategories = useSelector((state) => state.categories);
+
+  const { categories } = allcategories;
+
+  useEffect(() => {
+    setLoading(true);
+    const fetch = async () => {
+      let res = await axiosService.get(`${ENDPOINT}${GET_PRODUCTS_API}/${id}`);
+      dispatch(getInfoProducts(res.data));
+      setNewProduct(res.data);
+      setLoading(false);
+    };
+
+    fetch();
+  }, [dispatch, id]);
+
+  // componentDidMount
+  useEffect(() => {
+    dispatch(getAllCategories());
+  }, [dispatch]);
+
+  // get options for modal
+  const modalOptions = categories.map((item) => ({ ...item, value: item.id }));
+
+  // handle change input
+  const onChangeInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setNewProduct({
+      ...newProduct,
+      [name]: value,
     });
-    const dispatch = useDispatch();
-    const match = useRouteMatch();
-    const { t } = useTranslation("common");
-    const { id } = match.params;
-    const allcategories = useSelector((state) => state.categories);
+  };
+  // handle change select
+  const onChangeSelect = (value) => {
+    setNewProduct((prev) => {
+      return {
+        ...prev,
+        category: value,
+      };
+    });
+  };
 
-    const { categories } = allcategories;
-
-    useEffect(() => {
-        setLoading(true);
-        const fetch = async () => {
-            let res = await axiosService.get(`${ENDPOINT}${GET_PRODUCTS_API}/${id}`);
-            dispatch(getInfoProducts(res.data));
-            setNewProduct(res.data);
-            setLoading(false);
-        };
-
-        fetch();
-    }, [dispatch, id]);
-
-    // componentDidMount
-    useEffect(() => {
-        dispatch(getAllCategories());
-    }, [dispatch]);
-
-    // get options for modal
-    const modalOptions = categories.map((item) => ({ ...item, value: item.id }));
-
-    // handle change input
-    const onChangeInput = (e) => {
-        const name = e.target.name;
-        const value = e.target.value;
-        setNewProduct({
-            ...newProduct,
-            [name]: value,
-        });
+  // handle update
+  const handleUpdate = () => {
+    const error = {
+      ...isRequired("productName", newProduct.name),
+      ...isRequired("productCategory", newProduct.category),
+      ...isRequired("supplier", newProduct.supplier),
+      ...isRequired("description", newProduct.description),
+      ...isTypeNumber("price", newProduct.price),
+      ...isTypeNumber("quantity", newProduct.quantity),
     };
-    // handle change select
-    const onChangeSelect = (value) => {
-        setNewProduct((prev) => {
-            return {
-                ...prev,
-                category: value,
-            };
-        });
-    };
+    if (!_.isEmpty(error)) {
+      const errorMessage = _.uniq(Object.values(error));
+      errorMessage.map((item) => message.error(item));
+    } else {
+      const body = {
+        ...newProduct,
+      };
+      setLoading(true);
+      axiosService
+        .put(`${ENDPOINT}${GET_PRODUCTS_API}/${id}`, body)
+        .then((res) => {
+          message.success("Edit product successfully");
+          dispatch(UpdateProducts(res.data));
+        })
+        .catch((error) => {
+          message.error("Server Error");
+        })
+        .finally(() => setLoading(false));
+    }
+  };
+  return (
+    <div>
+      {loading && <Loading />}
 
-    // handle update
-    const handleUpdate = () => {
-        const error = {
-            ...isRequired("productName", newProduct.name),
-            ...isRequired("productCategory", newProduct.category),
-            ...isRequired("supplier", newProduct.supplier),
-            ...isRequired("description", newProduct.description),
-            ...isTypeNumber("price", newProduct.price),
-            ...isTypeNumber("quantity", newProduct.quantity),
-        };
-        console.log(error);
-        if (!_.isEmpty(error)) {
-            const errorMessage = _.uniq(Object.values(error));
-            errorMessage.map((item) => message.error(item));
-        } else {
-            const body = {
-                ...newProduct,
-            };
-            setLoading(true);
-            axiosService
-                .put(`${ENDPOINT}${GET_PRODUCTS_API}/${id}`, body)
-                .then((res) => {
-                    message.success("Edit product successfully");
-                    dispatch(UpdateProducts(res.data));
-                })
-                .catch((error) => {
-                    message.error("Server Error");
-                })
-                .finally(() => setLoading(false));
-        }
-    };
-    return (
-        <div>
-            {loading && <Loading />}
-
-            <div className="warehouse-detail">
-                <div className="content">
-                    <h2>{t(`warehouseDetail.title`)}</h2>
-                    <Row justify="space-between" style={{ paddingTop: "30px" }}>
-                        <Col xl={11} md={10} sm={12}>
-                            <ModalInput
-                                name="name"
-                                label={t("warehouseDetail.input.productName")}
-                                value={newProduct.name}
-                                onChange={(e) => onChangeInput(e)}
-                                placeholder="Input Product's Name"
-                                required
-                            />
-                        </Col>
-                        <Col xl={11} md={10} sm={12}>
-                            <ModalInput
-                                type="select"
-                                optionSelectValue={modalOptions}
-                                name="category"
-                                label={t("warehouseDetail.input.productCategory")}
-                                value={newProduct.category}
-                                onChange={onChangeSelect}
-                                placeholder="Input Product's Category"
-                                required
-                            />
-                        </Col>
-                    </Row>
-                    <Row justify="space-between" style={{ paddingTop: "30px" }}>
-                        <Col xl={11} md={10} sm={12}>
-                            <ModalInput
-                                name="price"
-                                label={t("warehouseDetail.input.price")}
-                                value={newProduct.price}
-                                onChange={(e) => onChangeInput(e)}
-                                placeholder="Input Product's Price"
-                                required
-                            />
-                        </Col>
-                        <Col xl={11} md={10} sm={12}>
-                            <ModalInput
-                                name="supplier"
-                                label={t("warehouseDetail.input.supplier")}
-                                value={newProduct.supplier}
-                                onChange={(e) => onChangeInput(e)}
-                                placeholder="Input Product's Supplier"
-                                required
-                            />
-                        </Col>
-                    </Row>
-                    <Row justify="space-between" style={{ paddingTop: "30px" }}>
-                        <Col xl={11} md={10} sm={12}>
-                            <ModalInput
-                                name="quantity"
-                                label={t("warehouseDetail.input.quantity")}
-                                value={newProduct.quantity}
-                                onChange={(e) => onChangeInput(e)}
-                                placeholder="Input Product's Quantity"
-                                required
-                            />
-                        </Col>
-                        <Col xl={11} md={10} sm={12}>
-                            <ModalInput
-                                type="textarea"
-                                name="description"
-                                label={t("warehouseDetail.input.description")}
-                                value={newProduct.description}
-                                onChange={(e) => onChangeInput(e)}
-                                placeholder="Input Product's Description"
-                                required
-                            />
-                        </Col>
-                    </Row>
-                    <Col xl={11} md={10} sm={12}>
-                        <Button type="primary" onClick={handleUpdate} htmlType="submit">
-                            {loading ? "Loading..." : t(`warehouseDetail.input.update`)}
-                        </Button>
-                    </Col>
-                </div>
-            </div>
+      <div className="warehouse-detail">
+        <div className="content">
+          <h2>{t(`warehouseDetail.title`)}</h2>
+          <Row justify="space-between" style={{ paddingTop: "30px" }}>
+            <Col xl={11} md={10} sm={12}>
+              <ModalInput
+                name="name"
+                label={t("warehouseDetail.input.productName")}
+                value={newProduct.name}
+                onChange={(e) => onChangeInput(e)}
+                placeholder="Input Product's Name"
+                required
+              />
+            </Col>
+            <Col xl={11} md={10} sm={12}>
+              <ModalInput
+                type="select"
+                optionSelectValue={modalOptions}
+                name="category"
+                label={t("warehouseDetail.input.productCategory")}
+                value={newProduct.category}
+                onChange={onChangeSelect}
+                placeholder="Input Product's Category"
+                required
+              />
+            </Col>
+          </Row>
+          <Row justify="space-between" style={{ paddingTop: "30px" }}>
+            <Col xl={11} md={10} sm={12}>
+              <ModalInput
+                name="price"
+                label={t("warehouseDetail.input.price")}
+                value={newProduct.price}
+                onChange={(e) => onChangeInput(e)}
+                placeholder="Input Product's Price"
+                required
+              />
+            </Col>
+            <Col xl={11} md={10} sm={12}>
+              <ModalInput
+                name="supplier"
+                label={t("warehouseDetail.input.supplier")}
+                value={newProduct.supplier}
+                onChange={(e) => onChangeInput(e)}
+                placeholder="Input Product's Supplier"
+                required
+              />
+            </Col>
+          </Row>
+          <Row justify="space-between" style={{ paddingTop: "30px" }}>
+            <Col xl={11} md={10} sm={12}>
+              <ModalInput
+                name="quantity"
+                label={t("warehouseDetail.input.quantity")}
+                value={newProduct.quantity}
+                onChange={(e) => onChangeInput(e)}
+                placeholder="Input Product's Quantity"
+                required
+              />
+            </Col>
+            <Col xl={11} md={10} sm={12}>
+              <ModalInput
+                type="textarea"
+                name="description"
+                label={t("warehouseDetail.input.description")}
+                value={newProduct.description}
+                onChange={(e) => onChangeInput(e)}
+                placeholder="Input Product's Description"
+                required
+              />
+            </Col>
+          </Row>
+          <Col xl={11} md={10} sm={12}>
+            <Button type="primary" onClick={handleUpdate} htmlType="submit">
+              {loading ? "Loading..." : t(`warehouseDetail.input.update`)}
+            </Button>
+          </Col>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default WarehouseDetailPage;
